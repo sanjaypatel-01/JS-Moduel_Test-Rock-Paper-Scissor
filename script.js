f81550b7c50ec28ab523807276c4916ea0383382
Sanjay Patel <sanjaypatel5346@gmail.com>
2024-09-23 18:11:47 +0530

    all features added, the only left is to add animations and to save the scores in local storage

--- script.js
+++ script.js
@@ -4,25 +4,90 @@ let compScore = 0;
 const rock = document.querySelector('#rock');
 const scissor = document.querySelector('#scissor');
 const paper = document.querySelector('#paper');
+const messagewin = document.querySelector('.message-win');
+
+const gameResults = document.querySelector('.game-results'); 
+const userPickedDisplay = document.querySelector('#userPicked'); 
+const pcPickedDisplay = document.querySelector('#pcPicked'); 
+const containerTwo = document.querySelector('.container-two');
+
+const ruleButton = document.querySelector('.btn-rules');
+const closeButton = document.querySelector('.cross');
+const ruleBox = document.querySelector('.rule-box');
+
+const playAgainButton = document.querySelector('.button-play-again');
+const nextButton = document.querySelector('.btn-next');
+const messageAgainstPC = document.querySelector('.message-against-pc');
+
+const displayGameWon = document.querySelector('.won-game');
+const scoreContainer = document.querySelector('.score-container');
+const buttonPlayAgainTwo = document.querySelector('.button-play-again-two');
 
 
-const drawGame = () => {
+// Initially hide the rules section, cross button section and game results
+ruleBox.style.display = 'none';
+closeButton.style.display = 'none';
+gameResults.style.display = 'none';
+nextButton.style.display = 'none';
+displayGameWon.style.display = 'none';
+
+// Show the rules and close button when the RULES button is clicked
+ruleButton.addEventListener('click', function() {
+    ruleBox.style.display = 'flex'; // Show the rules
+    closeButton.style.display = 'flex'; // Show the rules
+});
+
+// Hide the rules when the cross is clicked
+closeButton.addEventListener('click', function() {
+    ruleBox.style.display = 'none'; // Hide the rules
+    closeButton.style.display = 'none'; // Show the rules
+});
+
+
+
+const drawGame = (userChoice, computerChoice) => {
     console.log ('game was drawn');
+    messagewin.innerText = "TIE UP"
+    gameResults.style.display = 'flex'; 
+    nextButton.style.display = 'none';
+    
+    // Update the user's choice with the image for a tie
+    userPickedDisplay.innerHTML = `<div class="${userChoice}" id="${userChoice}"><img src="images/${userChoice}.png" alt="${userChoice}"></div>`;
+
+    // Update the computer's choice with the image for a tie
+    pcPickedDisplay.innerHTML = `<div class="${computerChoice}" id="${computerChoice}"><img src="images/${computerChoice}.png" alt="${computerChoice}"></div>`;
+
+    containerTwo.style.display = 'none'; // Hide game container
+    messageAgainstPC.style.display = 'none';
 }
 
-const showWinner = (userWin) => {
+const showWinner = (userWin, userChoice, computerChoice) => {
     if (userWin) {
         console.log('User Wins');
+        messagewin.innerText = "YOU WIN"
         userScore++;
         document.querySelector('#userScore').innerText = userScore;
+        nextButton.style.display = 'flex';
+
     } else {
         console.log('You lose');
+        messagewin.innerText = "YOU LOST"
         compScore++;
         document.querySelector('#compScore').innerText = compScore;
+        nextButton.style.display = 'none';
         // can also do like
         // updateScore = document.querySelector('#compScore')
         // updateScore.innerText = compScore;
     }
+    gameResults.style.display = 'flex'; // Show game results
+    messageAgainstPC.style.display = 'flex';
+    // Update the user's choice with the image
+    userPickedDisplay.innerHTML = `<div class="${userChoice}" id="${userChoice}"><img src="images/${userChoice}.png" alt="${userChoice}"></div>`;
+
+    // Update the computer's choice with the image
+    pcPickedDisplay.innerHTML = `<div class="${computerChoice}" id="${computerChoice}"><img src="images/${computerChoice}.png" alt="${computerChoice}"></div>`;
+    containerTwo.style.display = 'none'; // Hide game container
+    
 }
 
 const playGame = (userChoice) => {
@@ -32,7 +97,7 @@ const playGame = (userChoice) => {
 
     if (userChoice === computerChoice) {
         // Draw Game
-        drawGame();
+        drawGame(userChoice, computerChoice);
     } else {
         let userWin = true;
         if (userChoice === "rock") {
@@ -45,7 +110,7 @@ const playGame = (userChoice) => {
             // computer choice -> rock, paper
             userWin = computerChoice === "rock" ? false : true;
         }
-        showWinner(userWin);
+        showWinner(userWin, userChoice, computerChoice);
     } 
 }
 
@@ -74,3 +139,26 @@ paper.addEventListener('click', () => {
     //console.log('paper clicked', userChoice);
     playGame(userChoice);
 })
+
+playAgainButton.addEventListener('click', function() {
+    document.querySelector('#userScore').innerText = userScore;
+    document.querySelector('#compScore').innerText = compScore;
+    messagewin.innerText = "";
+    gameResults.style.display = 'none';
+    containerTwo.style.display = 'flex'; // Show game container
+    ruleBox.style.display = 'none';
+    closeButton.style.display = 'none';  // Hide rules and close button section
+});
+
+nextButton.addEventListener('click', () => {
+    displayGameWon.style.display = 'flex'; 
+    scoreContainer.style.display = 'none'; 
+    gameResults.style.display = 'none';
+
+});
+
+buttonPlayAgainTwo.addEventListener('click', () => {
+    displayGameWon.style.display = 'none'; 
+    scoreContainer.style.display = 'flex'; 
+    containerTwo.style.display = 'flex';
+});
